138b16a0732c1d89c46d267ee90e41f444869a3b
VeereshTarak <veeresh.k.qa@gmail.com>
2023-10-06 15:06:21 +0530

    this commit from Asian Timezone guy

--- src/Tutorail/Veereshacademy.java
+++ src/Tutorail/Veereshacademy.java
@@ -7,6 +7,8 @@ public class Veereshacademy {
 	@Test
 	public void Postinsta() {
 		System.out.println("Postinsta");
+		System.out.println("postfacebook");
+		System.out.println("postwhatsapp");
 	}
 	@Test
 	public void Deletepost() {
